2585e7ee3b80cfe77beed6d459983856c224836b
Nikhil Nayak <nikhil.nixel@gmail.com>
2021-09-15 19:43:13 +0530

    Initial Commit
    Includes:
     - Connect
     - Query

---
+++ index.ts
@@ -0,0 +1,51 @@
+import { createConnection, Socket } from 'net';
+import { v4 as uuid_v4 } from 'uuid';
+
+const DEFAULT_PORT = 4224;
+const DEFAULT_ADDRESS = 'localhost';
+const CRLF = '\r\n';
+
+interface IConnectPropType {
+  port?: number;
+  host?: string;
+}
+
+export class KeyStoneSock {
+  client?: Socket;
+  jobResolvers: Map<string, any>;
+
+  constructor() {
+    this.jobResolvers = new Map<string, any>();
+  }
+
+  async connect({ port = DEFAULT_PORT, host = DEFAULT_ADDRESS  }: IConnectPropType) {
+    return new Promise(
+      (resolve) => {
+        this.client = createConnection({
+          port,
+          host,
+        }, () => {
+          this.client?.on('data', (data: string) => {
+            const resp = data.toString().split(':');
+            let resolveInner = this.jobResolvers.get(resp[0]);
+            this.jobResolvers.delete(resp[0]);
+            resolveInner?.(resp[1]);
+          });
+          console.debug('Connected to Keystone');
+          resolve(this.client);
+        });
+      },
+    );
+  }
+
+  async query(queryString: string) {
+    const jobId = uuid_v4();
+
+    return new Promise<string>(
+      (resolve) => {
+        this.jobResolvers.set(jobId, resolve);
+        this.client?.write(`${jobId}:${queryString}${CRLF}`);
+      },
+    );
+  }
+}
---
+++ test/test.ts
@@ -0,0 +1,12 @@
+import { KeyStoneSock } from '../index';
+
+test('Query Test', async () => {
+  const ks = new KeyStoneSock();
+  await ks.connect({ port: 4224 });
+  const p1 = ks.query('SET a AS b WHERE TTL=10s NFETCH=2');
+  const p2 = ks.query('GET b');
+
+  console.log('OUT:', await p2);
+  console.log('OUT:', await p1);
+  expect(1).toBe(1);
+});
